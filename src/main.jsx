f43822890c6c998664ed7aef16ac73b4129b1cc6
Muluken Ugamo <Muler8905>
2025-10-30 06:10:19 -0700

    allow the change made in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
@@ -8,9 +10,10 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter  future={{ v7_relativeSplatPath: true }}>
-        <App />
-      </BrowserRouter>
+      <HashRouter >
+  <App />
+</HashRouter>
+
     </ThemeProvider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/muluken-portfolio_dev/', // <- replace with your repo name + trailing slash
   plugins: [react()],
 })
+
